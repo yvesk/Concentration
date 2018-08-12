1f3f36ec1cc8cc29f15414b6d2ef8585b6899575
Yves Kurz <Yves@fabiennes-macbook-pro.home>
2018-08-12 12:55:54 +0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Yves Kurz on 12.08.18.
+//  Copyright © 2018 Yves Kurz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
